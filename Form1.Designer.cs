292dbe29dc5015b11c9aa0ecff7426299b44d19e
DermengiIon <ion.dermengi859@gmail.com>
2021-07-07 01:52:05 +0300

    MUA  Mail User Agent

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.Email = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSub = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMsg = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.textBoxInbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnAttachment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(558, 350);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(23, 27);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(42, 17);
+            this.Email.TabIndex = 1;
+            this.Email.Text = "Email";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(188, 22);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(299, 22);
+            this.textBoxEmail.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Subject";
+            // 
+            // textBoxSub
+            // 
+            this.textBoxSub.Location = new System.Drawing.Point(188, 78);
+            this.textBoxSub.Name = "textBoxSub";
+            this.textBoxSub.Size = new System.Drawing.Size(299, 22);
+            this.textBoxSub.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Message";
+            // 
+            // textBoxMsg
+            // 
+            this.textBoxMsg.Location = new System.Drawing.Point(188, 167);
+            this.textBoxMsg.Multiline = true;
+            this.textBoxMsg.Name = "textBoxMsg";
+            this.textBoxMsg.Size = new System.Drawing.Size(299, 144);
+            this.textBoxMsg.TabIndex = 6;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(685, 350);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // textBoxInbox
+            // 
+            this.textBoxInbox.Location = new System.Drawing.Point(558, 57);
+            this.textBoxInbox.Multiline = true;
+            this.textBoxInbox.Name = "textBoxInbox";
+            this.textBoxInbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInbox.Size = new System.Drawing.Size(299, 254);
+            this.textBoxInbox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(555, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Inbox";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(808, 350);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 10;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnAttachment
+            // 
+            this.btnAttachment.Location = new System.Drawing.Point(188, 350);
+            this.btnAttachment.Name = "btnAttachment";
+            this.btnAttachment.Size = new System.Drawing.Size(107, 32);
+            this.btnAttachment.TabIndex = 11;
+            this.btnAttachment.Text = "Attachment";
+            this.btnAttachment.UseVisualStyleBackColor = true;
+            this.btnAttachment.Click += new System.EventHandler(this.btnAttachment_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 450);
+            this.Controls.Add(this.btnAttachment);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxInbox);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.textBoxMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSub);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSub;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMsg;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox textBoxInbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnAttachment;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_2
+{
+    public partial class Form1 : Form
+    {
+        private readonly SmtpClient Smtp;
+        private readonly OpenFileDialog FileDialog;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Smtp = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential()
+                {
+                    UserName = Account.User,
+                    Password = Account.Pass
+                },
+            };
+
+            FileDialog = new OpenFileDialog();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            MailAddress from = new MailAddress(Account.User, "Vanea");
+            MailAddress to = new MailAddress(textBoxEmail.Text, "Recipient");
+            MailMessage message = new MailMessage()
+            {
+                From = from,
+                Subject = textBoxSub.Text,
+                Body = textBoxMsg.Text,
+            };
+            message.To.Add(to);
+
+            if (FileDialog.FileName != "")
+            {
+                System.Net.Mime.ContentType contentType = new System.Net.Mime.ContentType();
+                contentType.MediaType = System.Net.Mime.MediaTypeNames.Application.Octet;
+                message.Attachments.Add(new Attachment(FileDialog.FileName, contentType));
+            }
+
+            try
+            {
+                Smtp.Send(message);
+                MessageBox.Show("Sent successfully");
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Something is wrong\n" + ex.Message, "Error");
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            using (var client = new ImapClient())
+            {
+                client.Connect("imap.gmail.com", 993, SecureSocketOptions.SslOnConnect);
+                client.Authenticate(Account.User, Account.Pass);
+                client.Inbox.Open(FolderAccess.ReadOnly);
+                var uids = client.Inbox.Search(SearchQuery.SubjectContains("lab2"));
+                textBoxInbox.ResetText();
+
+                foreach (var uid in uids)
+                {
+                    var message = client.Inbox.GetMessage(uid);
+                    message.WriteTo("inbox.txt");
+                    textBoxInbox.AppendText(string.Format("Subject: {0}{2}Body: {1}{2}{2}", message.Subject, message.TextBody, Environment.NewLine));
+                }
+
+                client.Disconnect(true);
+            }
+        }
+
+        private void btnAttachment_Click(object sender, EventArgs e)
+        {
+            if (FileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var size = new FileInfo(FileDialog.FileName).Length;
+                if (size > 1048576 * 2)
+                {
+                    MessageBox.Show("You cannot attach files larger than 2MB");
+                    FileDialog.FileName = "";
+                }
+            }
+        }
+    }
+}
